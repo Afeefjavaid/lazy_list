49a27e497dd867416888aaeb21055c0235c3a36d
afeef <afeefmalik111@gamil.com>
2025-02-27 22:14:47 +0500

    lazy grid from coursera

--- app/src/main/java/com/example/lazygrid/MainActivity.kt
+++ app/src/main/java/com/example/lazygrid/MainActivity.kt
@@ -8,7 +8,9 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
@@ -22,8 +24,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.lazygrid.ui.theme.LazyGridTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,25 +37,38 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             LazyGridTheme {
-                val state = rememberLazyGridState(initialFirstVisibleItemIndex = 99)
-              //  state.animateScrollToItem(index = 99, scrollOffset = 99)
-                LazyVerticalGrid(
-                    columns = GridCells.Adaptive(100.dp),
-                    state = state,
-                    content = {
-                        items(100){i ->
-                            Box(
-                                modifier = Modifier.padding(8.dp)
-                                    .aspectRatio(1f)
-                                    .clip(RoundedCornerShape(5.dp))
-                                    .background(Color.Green),
-                                contentAlignment = Alignment.Center
-                            ){
-                                Text("item $i")
-                            }
-                        }
+//                val state = rememberLazyGridState(initialFirstVisibleItemIndex = 99)
+//              //  state.animateScrollToItem(index = 99, scrollOffset = 99)
+//                LazyVerticalGrid(
+//                    columns = GridCells.Adaptive(100.dp),
+//                    state = state,
+//                    content = {
+//                        items(100){i ->
+//                            Box(
+//                                modifier = Modifier.padding(8.dp)
+//                                    .aspectRatio(1f)
+//                                    .clip(RoundedCornerShape(5.dp))
+//                                    .background(Color.Green),
+//                                contentAlignment = Alignment.Center
+//                            ){
+//                                Text("item $i")
+//                            }
+//                        }
+//                    }
+//                )
+//--------------------------------------------------------------------------------------------------
+            /*lazyList*/
+                LazyColumn {
+                    items(5000){
+                        Text("items $it",
+                            fontSize = 24.sp,
+                            fontWeight = FontWeight.Bold,
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier.fillMaxWidth()
+                                .padding(vertical = 8.dp)
+                            )
                     }
-                )
+                }
             }
         }
     }
